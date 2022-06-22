f7b583942dbdaed4ba87dd03dd9b4b196ea694f5
Dhruvi <drdoshi29@gmail.com>
2022-06-22 22:16:01 +0530

    add .env, .gitignore, requirements

--- Probe/src/data_collector.py
+++ Probe/src/data_collector.py
@@ -1,7 +1,9 @@
 import scapy.all as scapy
 import os
 import datetime
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def dataCapture(noOfPackets = 2,filename = "f"):
     ''' 
@@ -15,25 +17,38 @@ def dataCapture(noOfPackets = 2,filename = "f"):
         * none 
     '''
 
-    path = "../capture"
-    if not os.path.exists(path):   
-        os.makedirs(path)
+    path_capture = os.getenv('CAPTURED_PACKET_PATH')
+    if not os.path.exists(path_capture):  
+        try: 
+            os.makedirs(path_capture)
+        except(e):
+            print(e)
+            return e
 
-    path_packet = f"../capture/{filename}.pcap"
+    path_packet = "{}/{}.pcap".format(path_capture,filename)
 
-    capture = scapy.sniff(filter="ip6", count = noOfPackets)
-    scapy.wrpcap(path_packet,capture)
-    print(f"captured {noOfPackets} packets")
+    try:
+        capture = scapy.sniff(filter="ip6", count = noOfPackets)
+        print("captured {} packets".format(noOfPackets))
+    except(e):
+        print(e)
+        return e
+    
+    try:
+        scapy.wrpcap(path_packet,capture)
+        print("Saved the captured packets as {}.pcap".format(filename))
+    except(e):
+        print(e)
+        return e
 
     myhost = os.uname().nodename
-    os.setxattr(path_packet,'user.hostname',myhost.encode('ascii'))
-    print(f"captured by: {myhost}")
+    os.setxattr(path_packet,'user.hostname',myhost.encode('ascii')) #change
+    print("captured by: {}".format(myhost))
 
-    now = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    now = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")    #change
     os.setxattr(path_packet,'user.date',now.encode('ascii'))
-    print(f"captured on {now}")
+    print("captured on {}".format(now))
 
-    print("------------------------\n")
 
 
 
