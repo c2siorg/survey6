f441fea9261425f0f4eef8a57d69002a7ebdb5a9
Dhruvi <drdoshi29@gmail.com>
2022-07-21 23:10:03 +0530

    alias edit and commenting

--- C&C Server/setup.bash
+++ C&C Server/setup.bash
@@ -1,26 +1,18 @@
 #!/bin/bash
 
+
+# Installation of requirements in virtual env
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
+
+
+# Creating a service 
 sudo cp server.service /etc/systemd/system/survey6_server.service
 
+systemctl daemon-reload
 
-function cncserver 
-{ 
-    if [ -z "$1" ];
-    then
-        echo "Enter a command: start / stop"
-    elif [ "$1" = "start" ]; 
-    then 
-        systemctl start survey6_server
-    elif [ "$1" = "stop" ]; 
-    then 
-        systemctl stop survey6_server
-    else
-        echo "Command not supported"
-    fi 
-}
+# Aliasing 
+cat cnc_server.bash_aliases >> ~/.bash_aliases 
 
-alias cncserver=cncserver
-
+source ~/.bashrc
