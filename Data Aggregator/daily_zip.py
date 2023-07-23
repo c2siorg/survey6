58743df4c23dc8fefcc2fc72b59180ce2c591a0c
dhruvi29 <drdoshi29@gmail.com>
2023-07-23 20:17:38 +0530

    hourly and daily zip

--- Data Aggregator/daily_zip.py
+++ Data Aggregator/daily_zip.py
@@ -8,32 +8,6 @@ import sys
 input_directory = config.BACKUP_PATH
 output_directory = config.RAW_PCAP_FILES
 
-def create_hourly_zip2():
-    for clientDir in os.listdir(input_directory):
-        clientInputDir = os.path.join(input_directory,clientDir) 
-        clientProcessDir = os.path.join(output_directory,clientDir) 
-        hourly_zip_directory = os.path.join(clientProcessDir,"hourly")
-
-        if not os.path.exists(hourly_zip_directory):  
-            try: 
-                os.makedirs(hourly_zip_directory)
-            except OSError as e:
-                sys.exit(0)
-
-        now = datetime.datetime.now()
-        zip_filename = f"{now.strftime('%Y-%m-%d_%H-%M')}.zip"
-
-        if(len(os.listdir(clientInputDir)) == 0):
-             print("empty dir")
-             return
-
-        with zipfile.ZipFile(os.path.join(hourly_zip_directory, zip_filename), "w") as zipf:
-            for file in os.listdir(clientInputDir):
-                if(file.startswith(now.strftime('f%d-%m-%Y-%H'))):
-                    file_path = os.path.join(clientInputDir,file)
-                    zipf.write(file_path,file)
-                    os.remove(file_path)
-                
 def create_daily_zip():
 
     yesterday = datetime.datetime.now() - datetime.timedelta (days=1)
@@ -59,8 +33,5 @@ def create_daily_zip():
                         file_path = os.path.join(root, file)
                         zipf.write(file_path, os.path.relpath(file_path, hourly_zip_directory))
 
-
-
-# create_hourly_zip2()
-create_daily_zip()
-# print(os.walk(input_directory))
+if __name__ == "__main__":
+    create_daily_zip()
