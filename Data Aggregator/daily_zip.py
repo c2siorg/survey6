6714c9bdabd63972d274f9963c54e0235b9a0df0
dhruvi29 <drdoshi29@gmail.com>
2023-08-06 08:06:49 +0530

    deleting hourly files

--- Data Aggregator/daily_zip.py
+++ Data Aggregator/daily_zip.py
@@ -32,6 +32,8 @@ def create_daily_zip():
                     if file.endswith(".zip") and file.startswith(yesterday.strftime('%Y-%m-%d')):
                         file_path = os.path.join(root, file)
                         zipf.write(file_path, os.path.relpath(file_path, hourly_zip_directory))
+                        os.remove(file_path)
+
 
 if __name__ == "__main__":
     create_daily_zip()
